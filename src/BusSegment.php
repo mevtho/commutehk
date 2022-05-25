2797cd5643ba5397d8823f390a2fcb679a839f59
Thomas MEVEL <tmevel@mevtho.com>
2022-05-25 19:00:53 +0800

    Fixing bus time sort

--- src/BusSegment.php
+++ src/BusSegment.php
@@ -36,7 +36,7 @@ class BusSegment extends Segment
                     ...$response->data
                 ];
             }
-            uasort($this->data[$route], fn($a, $b) => strcmp($a->eta, - $b->seq));
+            uasort($this->data[$route], fn($a, $b) => strcmp($a->eta, $b->eta));
         }
     }
 
